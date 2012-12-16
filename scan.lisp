da7b65ab931cb8c142b0a9052b43c6a16b1af8fa
Yoshinori Tahara <read.eval.print@gmail.com>
2012-12-16 13:10:18 +0900

    $lt とか。

--- scan.lisp
+++ scan.lisp
@@ -7,7 +7,10 @@
                                                      order)
   (let ((docs (find-object (apply #'cl-mongo:kv
                                   (cl-mongo:kv (symbol-to-key +class+) (serialize (class-name class)))
-                                  (let ((kv (compute-where (car where) (cdr where))))
+                                  (let ((kv (compute-where
+                                             (aand (car where)
+                                                  ( intern (symbol-name it) :keyword))
+                                             (cdr where))))
                                     (if kv
                                         (list kv)
                                         nil))))))
@@ -22,8 +25,23 @@
 (defmethod compute-where ((op null) args)
   nil)
 
-(defmethod compute-where ((op (eql '=)) args)
+(defmethod compute-where ((op (eql :=)) args)
   (destructuring-bind (slot-name value) args
     (cl-mongo:kv (symbol-to-key slot-name) (serialize value))))
 
+(defmethod compute-where ((op (eql :in)) args)
+  (destructuring-bind (slot-name . values) args
+    (cl-mongo:kv (symbol-to-key slot-name)
+                 (cl-mongo:kv "$in" (mapcar #'serialize values)))))
+
+(macrolet ((m (op-key op)
+             `(defmethod compute-where ((op (eql ,op-key)) args)
+                (destructuring-bind (slot-name value) args
+                  (cl-mongo:kv (symbol-to-key slot-name)
+                               (cl-mongo:kv ,op (serialize value)))))))
+  (m :< "$lt")
+  (m :> "$gt")
+  (m :<= "$lte")
+  (m :>= "$gte")
+  (m :!= "$ne"))
 
